02be34a0c5ad694553d4d00d10dfa90024da2879
Eduardo Valle <eduardolaccayo@gmail.com>
2023-09-06 19:59:51 -0600

    cambios a fusion y css con los doccumentos de clase 8

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector(".email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const BarMobile = document.querySelector(".menu");
+const ListMobile = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+BarMobile.addEventListener("click", toggleListMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+}
+
+
+function toggleListMenu(){
+    ListMobile.classList.toggle("inactive")
 }
