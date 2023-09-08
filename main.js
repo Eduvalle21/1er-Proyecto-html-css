e42864006e6aeccb302f276894b9c42536932498
Eduardo Valle <eduardolaccayo@gmail.com>
2023-09-08 16:49:56 -0600

    Cambios a fusiones main y styles

--- main.js
+++ main.js
@@ -1,16 +1,59 @@
 const menuEmail = document.querySelector(".email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuCarritoIcon = document.querySelector(".shopping-cart-navright");
 const BarMobile = document.querySelector(".menu");
 const ListMobile = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 BarMobile.addEventListener("click", toggleListMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
+
+
 
 function toggleDesktopMenu(){
+    const asideclose = aside.classList.contains("inactive");
+
+
+    if(!asideclose){
+        aside.classList.add("inactive");
+        
+    }
+
     desktopMenu.classList.toggle("inactive");
 }
 
 
 function toggleListMenu(){
-    ListMobile.classList.toggle("inactive")
+    const asideclose = aside.classList.contains("inactive");
+
+
+    if(!asideclose){
+        aside.classList.add("inactive");
+        
+    }
+
+    ListMobile.classList.toggle("inactive");
+}
+
+
+
+
+function toggleCarritoAside(){
+    const mobileMenuClose = ListMobile.classList.contains("inactive");
+    const menuDesktopclose = desktopMenu.classList.contains("inactive")
+
+
+    if(!mobileMenuClose){
+        ListMobile.classList.add("inactive");
+        
+    }
+    if(!menuDesktopclose){
+        desktopMenu.classList.add("inactive");
+        
+    }
+
+    aside.classList.toggle("inactive");
+
+
 }
